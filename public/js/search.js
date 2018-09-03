d93e65090cbc8bf27e787c6e84e42bfb632ab2a7
“Ivan” <Ivan.rodriguez@densitylabs.io>
2018-09-03 09:05:15 -0500

    fixing front-end

--- public/js/search.js
+++ public/js/search.js
@@ -32,11 +32,13 @@ function searchToApi(val){
 }
 
 (function(){
-    console.log(window.location);
     if (window.location.pathname === "/galeria"){
         $.get('/api/search_by_tag' + window.location.search, function(data){
             data = JSON.parse(data);
-            console.log(data);
+            var container = document.getElementById("example");
+            for(var i in data){
+              container.innerHTML += '<div class="galery-container"><a href="/vista" style="text-decoration:none;"><img class="featurette-image img-responsive center-block" src="'+ data[i].imagenes[0]+'" alt="Generic placeholder image">\<div class="info-car"><h2 id="results13"></h2><h2>&nbsp;</h2> <h2 id="results14">'+ data[i].Marca+'<span>'+data[i].modelo+'</span></h2><h3 id="results12">$ '+ data[i].precio+'</h3><h2 id="results1"></h2><h2>&nbsp;</h2> <h2 id="results2"></h2></div></a></div>'
+            }
         })
     } else {
         return false;
