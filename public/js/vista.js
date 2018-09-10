faf67964c1a56b30f4ff5dccc7fc70c6dfd605f0
ivanxtr <32345257+ivanxtr@users.noreply.github.com>
2018-09-09 22:48:31 -0500

    fixing vista and js (#7)

--- public/js/vista.js
+++ public/js/vista.js
@@ -15,11 +15,31 @@ sBtn.addEventListener('click', function(){
     if(param.length > 1){
         $.get('/api/search_by_id?id='+ param[1]+'', function(data){
             data = JSON.parse(data);
-            var container = document.getElementById("example");
-            console.log(data);
-            for(var i in data){
-                container.innerHTML += '<div class="galery-container"><a href="/vista?id='+ data[i].id+'" style="text-decoration:none;"><img class="featurette-image img-responsive center-block" src="'+ data[i].imagenes[0]+'" alt="Generic placeholder image">\<div class="info-car"><h2 id="results13"></h2><h2>&nbsp;</h2> <h2 id="results14">'+ data[i].Marca+'<span>'+data[i].modelo+'</span></h2><h3 id="results12">$ '+ data[i].precio+'</h3><h2 id="results1"></h2><h2>&nbsp;</h2> <h2 id="results2"></h2></div></a></div>'
+            var carousel = document.getElementsByClassName('carousel-inner')[0];
+            if(data[0] !== undefined){
+                data[0].year !== null ?document.getElementById('results1').innerHTML = data[0].year : "";
+                data[0].Kilometraje !== null ? document.getElementById('results2').innerHTML = data[0].Kilometraje : "";
+                data[0].Motor_desc != null ? document.getElementById('results3').innerHTML = data[0].Motor_desc : "";
+                data[0].color !== null ? document.getElementById('results4').innerHTML = data[0].color : "";
+                data[0].interiores !== null ? document.getElementById('results5').innerHTML = data[0].interiores : "";
+                data[0].Frenos !== null ? document.getElementById('results6').innerHTML = data[0].Frenos : "";
+                data[0].cilindraje !== null ? document.getElementById('results7').innerHTML = data[0].cilindraje : "";
+                data[0].traccion !== null ? document.getElementById('results8').innerHTML = data[0].traccion : "";
+                data[0].transmision !== null ? document.getElementById('results9').innerHTML = data[0].transmision : "";
+                data[0].quemacocos !== null ? document.getElementById('results10').innerHTML = data[0].quemacocos : "";
+                data[0].electrico !== null ? document.getElementById('results11').innerHTML = data[0].electrico : "";
+                if(data[0].imagenes != undefined){
+                    for (var i in data[0].imagenes){
+                        if( i == 0){
+                            carousel.innerHTML= '<div class="item active"><img class="first-slide" src="'+ data[0].imagenes[i]+'" alt="First slide">"</div>'
+                        } else {
+                            carousel.innerHTML+= '<div class="item"><img class="first-slide" src="'+ data[0].imagenes[i]+'" alt="First slide">"</div>'
+                        }
+
+                    }
+                }
             }
+
         })
     } else {
         return false;
